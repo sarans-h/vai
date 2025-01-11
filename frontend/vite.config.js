4b080d094991a0bb801326e2b34cfc977ad96c1e
Saransh Gupta <113707195+sarans-h@users.noreply.github.com>
2025-01-12 00:51:02 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://vai-2ucd.onrender.com',
         changeOrigin: true,
       },
     },
