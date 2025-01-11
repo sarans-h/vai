914006aceff1be4266112bd4733ddf7dcabbfa75
Saransh Gupta <113707195+sarans-h@users.noreply.github.com>
2025-01-12 01:49:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://vai-six.vercel.app', // Update with your frontend's origin
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
